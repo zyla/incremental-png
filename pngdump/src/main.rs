b608c997f2ebd1eec017ccedb3efc3e096ec9bf8
Maciej Bielecki <zyla@prati.pl>
2023-10-29 12:22:53 +0100

    pngdump: Add --print-sizes

--- pngdump/src/main.rs
+++ pngdump/src/main.rs
@@ -11,6 +11,9 @@ struct Args {
 
     #[arg(long, default_value_t = 1024)]
     input_buffer_size: usize,
+
+    #[arg(long)]
+    print_sizes: bool,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -20,6 +23,16 @@ fn main() -> anyhow::Result<()> {
     let mut dechunker = Dechunker::new();
     let mut sd = StreamDecoder::new();
     let mut inflater = Inflater::<1024>::new();
+
+    if args.print_sizes {
+        println!("Memory usage:");
+        println!("  Dechunker: {}", std::mem::size_of::<Dechunker>());
+        println!("  StreamDecoder: {}", std::mem::size_of::<StreamDecoder>());
+        println!(
+            "  Inflater (output buffer=1024): {}",
+            std::mem::size_of::<Inflater<1024>>()
+        );
+    }
 
     let mut buf = vec![0u8; args.input_buffer_size];
     loop {
